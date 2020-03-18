a33a8f855f8593bb4d3e34847643497b00b70b9b
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2020-03-17 20:07:05 -0600

    Convert file to image

--- example-codes/file2image/file2img.py
+++ example-codes/file2image/file2img.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
+from PIL import Image
 import numpy as np
-from skimage import color
 
 
 def file2image(filepath):
@@ -15,6 +14,8 @@ def file2image(filepath):
             for j in range(cols):
                 row.append(int.from_bytes(content[4 + i * cols + j:4 + i * cols + j + 1], byteorder='big', signed=False))
             img.append(np.array(row).astype(np.uint8))
+        img = np.array(img, dtype=np.uint8)
+        img = Image.fromarray(img)
         img.save(filepath[:-4] + '.jpg')
 
 
